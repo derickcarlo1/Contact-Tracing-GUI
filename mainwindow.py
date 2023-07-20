83ea32d030fd8357c8bde0007f9c682c559e1cc0
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-07-20 17:16:32 +0800

    plotting pseudos

---
+++ mainwindow.py
@@ -0,0 +1,21 @@
+# pip install tkinter and pillow
+
+# Function to save the collected information to a file
+
+# Function to search for an entry by name and display the information
+
+# Create the main app window
+
+# Background Image
+
+# Variables for user information
+
+# User Information Input
+
+# Save Button
+
+# Search Entry
+
+# Search Result
+
+# Run the main loop
