bc651547bbbc69c33dd5589cbb39fa45843a61cd
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-07-20 19:45:53 +0800

    func to save data and created the main app window

--- mainwindow.py
+++ mainwindow.py
@@ -1,10 +1,34 @@
-# pip install tkinter and pillow
+import tkinter as tk
+from tkinter import messagebox
+from PIL import Image, ImageTk
 
 # Function to save the collected information to a file
+def save_data():
+    name = name_var.get()
+    age = age_var.get()
+    gender = gender_var.get()
+    contact = contact_var.get()
+    address = address_var.get()
+    travel_history = travel_history_var.get()
+    symptoms = symptoms_var.get()
+
+    with open("contact_tracing.txt", "a") as file:
+        file.write(f"Name: {name}\n")
+        file.write(f"Age: {age}\n")
+        file.write(f"Gender: {gender}\n")
+        file.write(f"Contact: {contact}\n")
+        file.write(f"Address: {address}\n")
+        file.write(f"Travel History: {travel_history}\n")
+        file.write(f"Symptoms: {symptoms}\n")
+        file.write("\n")
+    messagebox.showinfo("Success", "Data saved successfully!")
 
 # Function to search for an entry by name and display the information
 
 # Create the main app window
+app = tk.Tk()
+app.title("Contact Tracing App")
+app.geometry("600x600")
 
 # Background Image
 
